2d6d5902ee1b403299742fe8dd1d6928e3ff9cc7
aleksandr krol <aleksandr.krol@uvoteam.com>
2019-01-31 20:32:25 +0200

    Add check arguments size check for example/main.c
    Signed-off-by: aleksandr krol <aleksandr.krol@uvoteam.com>

--- example/main.c
+++ example/main.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 int main(int argc, char *argv[]) {
     char *buffer = NULL;
 
     char * src = NULL;
     size_t length;
+    assert(argc == 2);
     FILE * f = fopen (argv[1], "rb");
 
     if (f)
