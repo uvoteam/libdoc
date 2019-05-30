1221963deb17a4b928c242430ea1fb2b9358548c
aleksandr krol <aleksandr.krol@uvoteam.com>
2019-05-30 19:21:00 +0300

    remove importlib from test.py
    Signed-off-by: aleksandr krol <aleksandr.krol@uvoteam.com>

--- python/test.py
+++ python/test.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 import extract_doc
-import importlib
-importlib.reload(extract_doc)
-with open('./test3.doc', 'rb') as myfile:
+
+with open('./test.doc', 'rb') as myfile:
       data = bytearray(myfile.read())
       print(extract_doc.extract_doc_text(data, len(data)))
